2d4ba39a5802fc84d2b51a8b9e1af5e824330f71
Aleksandar Spasovski <48162180+aleksandarspasovski@users.noreply.github.com>
2020-08-18 12:06:54 +0200

    Update Database.php

--- route/Database.php
+++ route/Database.php
@@ -5,7 +5,7 @@ class Database
 	public $db;
 	function __construct()
 	{
-        $db = new mysqli('127.0.0.1', 'root', 'homosapiens', 'factoryww', '3308');
+        $db = new mysqli('127.0.0.1', 'root', '', 'factoryww', '3308');
 		$this->db = $db;
     }
 }
